2261568559e764e8d1ce30dddcf99098f193dfb1
Vinita Rane <vinitarane1998@gmail.com>
2021-11-09 17:35:48 +0530

    lgin

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap/dist/css/bootstrap.css";
 import { Link } from "react-router-dom";
 import "./Login.css";
 
